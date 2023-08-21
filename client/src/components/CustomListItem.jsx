18b06c177e12c493974cf502dc61113d916d6dbc
JosefTheKing17 <yosimami50@gmail.com>
2023-08-21 05:16:43 +0300

    Try to use netlify for deploy

--- client/src/components/CustomListItem.jsx
+++ client/src/components/CustomListItem.jsx
@@ -4,9 +4,8 @@ import {
   Typography,
   ListItemSecondaryAction,
   ListItemText,
-  Button,
 } from "@mui/material";
-import React, { useEffect } from "react";
+import React from "react";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import { useDispatch } from "react-redux";
@@ -37,6 +36,7 @@ const CustomListItem = ({ item, refetchItems }) => {
     dispatch(decrementTotalItems());
     refetchItems();
   };
+
 
   return (
     <ListItem
